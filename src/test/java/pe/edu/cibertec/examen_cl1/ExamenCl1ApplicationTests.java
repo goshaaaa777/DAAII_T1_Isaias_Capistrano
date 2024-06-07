180ad3bf93a7dd713eac330b75a48403bcb5ccce
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-07 11:19:18 -0500

    primer commit

---
+++ src/test/java/pe/edu/cibertec/examen_cl1/ExamenCl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examen_cl1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenCl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
